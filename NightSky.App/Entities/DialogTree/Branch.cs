7accc7de7382a04b75b4564ad965325587cd1a7d
unknwxxxx <unknwxxx@gmail.com>
2023-03-06 18:22:09 +0300

    DialogTreeRefactoring

---
+++ NightSky.App/Entities/DialogTree/Branch.cs
@@ -0,0 +1,34 @@
+﻿namespace NightSky.App.Entities.DialogTree
+{
+    [Serializable]
+    public class Branch
+    {
+        public string Text { get; set; }
+        public List<Option> Content { get; set; }
+
+        public Branch()
+        {
+            Content = new List<Option>();
+        }
+        public Branch(string text)
+        {
+            Text = text;
+            Content = new List<Option>();
+        }
+
+        public Branch AddOption(string optionText, Branch nextBranch, int index)
+        {
+            Content.Add(new Option(optionText, nextBranch, index));
+            return this;
+        }
+        public Branch RemoveOption(int index)
+        {
+            Content = Content.Where((c, i) => i != index).ToList();
+            return this;
+        }
+    }
+}
+
+
+
+
