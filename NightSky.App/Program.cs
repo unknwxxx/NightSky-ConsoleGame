339843bcf4bed388c9fca2d844473ed07448a136
unknwxxxx <unknwxxx@gmail.com>
2023-03-08 20:05:59 +0300

    merge-develop-(cleaning)

--- NightSky.App/Program.cs
+++ NightSky.App/Program.cs
@@ -1,13 +1,7 @@
 ﻿using NightSky.App.Entities.DialogTree;
 using System.Text.Json;
 
-//name, age, race, gender, state, maxHealth, health, canTalk, canMove - Mage
-
-//var player = new Mage("Адольф", 10, Race.Orc, Gender.Male, 
-//State.Sick, 100f, 50f, true, true, 40f, 100f);
-
 DialogTree dialogTree = new DialogTree();
-
 
 
 using (FileStream fs = new FileStream("D:\\NightSky-ConsoleGame\\NightSky.App\\Files\\test.json",
